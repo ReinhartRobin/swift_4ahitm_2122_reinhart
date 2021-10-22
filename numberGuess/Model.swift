c317884045a511cfa971cdce9af4921f19e4030b
Reinhart Robin <it180215@WS8A02.htl-leonding.ac.at>
2021-10-22 09:39:56 +0200

    add program

---
+++ numberGuess/Model.swift
@@ -0,0 +1,32 @@
+class Model{
+    var numberToGuess = 0
+    var countOfTries = 0
+    
+    func compare(guess guessedNumber:Int!) -> Int! {
+        var result = 0
+        
+        if guessedNumber > numberToGuess {
+            result = 1
+        }else if guessedNumber < numberToGuess{
+            result = -1
+        }
+        return result
+    }
+    
+    
+    func compare(guessedString:String!) -> Int!{
+        let guess = Int(guessedString)
+        return compare(guess: guess)
+    }
+    
+    func isValidGuess(value:Int!) -> Bool{
+        if let number = value {
+            if number > 0 && number < 100 {
+                return true
+            }
+            return false
+        }else{
+            return false
+        }
+}
+}
